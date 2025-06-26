bb83c82ee0a11bd38826819889946ba70566ea8f
Jean Carlos Oliveira Lima <jcoliveiralima19@gmail.com>
2025-06-26 16:30:20 -0300

    Resolvendo sistemas de clique direito e esquerdo adicionarem cores diferentes

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const elementosTabela = document.querySelectorAll(".elemento-tabela");
 
+function preencher(elemento) {
+    elemento.classList.add('preenchido');
+}
+
+function despreencher(elemento) {
+    elemento.classList.remove('preenchido');
+}
+
 elementosTabela.forEach(elemento => {
-    elemento.addEventListener("mousedown", () => {
-        elemento.classList.add('preenchido');
-        console.log(elemento);
-    })
+    elemento.addEventListener('mousedown', function (e) {
+        e.preventDefault(); 
+        
+        if (e.button === 0) {
+            preencher(this);
+        } else if (e.button === 2) {
+            despreencher(this);
+        }
+    });
 
     elemento.addEventListener('mouseover', function (e) {
         if (e.buttons === 1) {
-            elemento.classList.add('preenchido');
-            console.log(elemento);
+            preencher(this);
+        } else if (e.buttons === 2) {
+            despreencher(this);
         }
     });
+
+    elemento.addEventListener('contextmenu', e => e.preventDefault());
 });
